225b4a69b5559fc52d9399837cfea2d7dab3191f
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-04-21 19:54:08 -0300

    random star

--- PF_Client/src/components/StarRank/StarRank.jsx
+++ PF_Client/src/components/StarRank/StarRank.jsx
@@ -3,7 +3,7 @@ import { BsStar, BsStarFill } from "react-icons/bs";
 import styles from "../StarRank/StarRank.module.css"
 
 const StarRank = (props)=>{
-  const score = 0;
+  const score = Math.floor(Math.random() * (5 - 1 + 1) + min);
 
   return(
     <div className={styles.stars}>
